466fb1b9f413f74f511246322abb19e2d93e8be0
salimi069 <hun.saller@Qgmail.com>
2023-11-19 21:38:06 +0100

    Configuring DB connection

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
+        'hostname'     => '172.17.0.1:3306',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'hotels',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
