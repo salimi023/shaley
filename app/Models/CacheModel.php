12a04ee70a522d3091f775709d9d9f2a8a508335
salimi069 <hun.saller@Qgmail.com>
2023-11-19 21:41:35 +0100

    DB Model file

---
+++ app/Models/CacheModel.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class CacheModel extends Model
+{
+    
+    // Table data
+    protected $DBGroup = 'default';
+
+    protected $table = 'cache';
+    protected $primaryKey = 'offer_id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+
+    // Columns
+    protected $allowedFields = [
+        'hotel_id',
+        'hotel_name',
+        'price',
+        'source',
+        'country_id',
+        'country',
+        'city_id',
+        'city',
+        'zip',
+        'address',
+        'latitude',
+        'longitude',
+        'star',
+        'image'
+    ];
+
+    // Validation
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = true;
+    protected $cleanValidationRules = false;
+}
