bbe6caf5cc408f5dca4d12960927aab9967167c4
salimi069 <hun.saller@Qgmail.com>
2023-11-22 00:02:32 +0100

    Adding controller code

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,7 +9,8 @@ use Psr\Log\LoggerInterface;
 class Home extends BaseController
 {    
     // DB Models
-    private $cache_model = false;    
+    private $cache_offers_model = false;
+    private $db = false;    
 
     // Constructor
     public function initController(
@@ -19,16 +20,17 @@ class Home extends BaseController
     ) 
     {
         parent::initController($request, $response, $logger);
-        $this->cache_model = new \App\Models\CacheModel;        
+        $this->cache_offers_model = new \App\Models\CacheModel;
+        $this->db = \Config\Database::connect('default');        
     }    
     
     public function index(): string
     {
-        $saved_offers = $this->cache_model->findAll();
+        $result = false;
+        $saved_offers = $this->cache_offers_model->findAll();
+        $curl = \Config\Services::curlrequest();        
 
-        if(count($saved_offers) == 0) {        
-        
-            $curl = \Config\Services::curlrequest();
+        if(count($saved_offers) == 0) {                           
 
             // API call
             $offer_data_json = $curl->request('get', 'http://testapi.swisshalley.com/hotels/',
@@ -63,11 +65,55 @@ class Home extends BaseController
                         'image' => $offer['image']
                     ];
 
-                    $save_offer = $this->cache_model->insert($offer_data, false);
+                    $save_offer = $this->cache_offers_model->insert($offer_data, false);
                 }
+
+                $result = $this->pull_offers_data();
             }                                   
-        }                   
+        } else {                                                            
+            $result = $this->pull_offers_data();          
+        }                
         
-        return view('home');
+        // Adding image URL if not exists
+        if($result) {                                    
+            foreach($result as $index => $array) {
+                if(empty($array['image'])) {
+                    $hotel_id = $array['hotel_id'];                    
+                    $find_image_sql = "SELECT offer_id, hotel_name, `image` FROM cache_offers WHERE hotel_id = $hotel_id AND `image` IS NOT NULL";
+                    $image_url = $this->db->query($find_image_sql)->getResult('array');                                                                              
+                    
+                    // Checking the HTTP status code of image
+                    if(!empty($image_url)) {
+                        foreach($image_url as $url) {                            
+                            $image_url_test = $curl->request('get', $url['image'], ['http_errors' => false]);
+                            $http_response_code = $image_url_test->getStatusCode();                            
+
+                            if($http_response_code == 200) {
+                                $result[$index]['image'] = $url['image'];
+                                break;
+                            } 
+                        }
+                    }
+                }
+            }            
+
+            $offers_data['offers'] = $result; 
+        } else {
+            $offers_data['offers'] = [];
+        }                                         
+        
+        return view('home', $offers_data);        
+    }
+
+    /**
+     * Pulling offers data from database 
+     * @return array
+     */    
+    private function pull_offers_data() 
+    {        
+        $sql = "SELECT hotel_id, hotel_name, country, city, min(price) AS price, `image`, star FROM cache_offers 
+        GROUP BY hotel_id";                                       
+            
+        return $this->db->query($sql)->getResult('array');
     }
 }
